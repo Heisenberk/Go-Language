5550169d29561e43b246a0e5f61055f1b3983a88
Mehdi Merimi <mehdi.78340@hotmail.fr>
2019-01-23 21:48:33 +0100

    finission de interface

--- exemple/interface/interface.go
+++ exemple/interface/interface.go
@@ -1,17 +1,25 @@
 package main
 
-
+import "fmt"
 
 type Equipement interface {
-equiper() int8
+porter() int8
 }
 
 type Soldat struct {
 speed,endurance int8
 }
 
+func (s Soldat) porter() int8 {
+return s.speed - 2
+}
+func mesure(e Equipement){
+fmt.Println(e)
+fmt.Println("speed",e.porter())
 
+}
 func main() {
-
+s := Soldat{speed : 5, endurance : 3}
+mesure(s)
 
 }
