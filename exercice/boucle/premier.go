8453ae1afb8c045b411b5e15704c1d267d669bd1
Mehdi Merimi <mehdi.78340@hotmail.fr>
2019-01-25 22:47:46 +0100

    ajout de l'exercice nombre premier

---
+++ exercice/boucle/premier.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+
+func main() {
+n := 97
+for i := 2 ; i < n ; i++ {
+if n % i == 0{
+fmt.Println("n n'est pas premier")
+break
+}
+}
+fmt.Println("n est premier")
+}
