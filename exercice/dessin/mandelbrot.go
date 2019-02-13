45ae177033ad03eeee92084380f190285a07abae
Mehdi Merimi <mehdi.78340@hotmail.fr>
2019-02-13 14:55:06 +0100

    rendre le code mandelbrot plus propre et ajout de constante

--- exercice/dessin/mandelbrot.go
+++ exercice/dessin/mandelbrot.go
@@ -9,55 +9,74 @@ import (
     "math/cmplx"
 )
 
+const(
+
+    Bordure = 600
+
+    Xmin = -2
+
+    Xmax = 2
+
+    Ymin = -2
+
+    Ymax = 2
+
+    ItérationMax = 100
+
+    BorneModule = 2
+)
+
 func mandelbrot(c complex128) color.Color {
 
    var z complex128
 
-               for n := 0; n< 50; n++ {
-            z=z*z+c
-            if cmplx.Abs(z)>2 {
+    for n := 0; n< ItérationMax; n++ {
+
+        z=z*z+c
+
+        if cmplx.Abs(z)> BorneModule {
             
             return color.RGBA{255,255,255,255}
         }
     }
-        return color.Black
+
+    return color.Black
 
 }
+
 
 func main() {
 new_png_file := "/home/user/Bureau/Go-Language/exercice/dessin/mandelbrot.png"
 
-
-
-
-
-
     bordure:= 600
+
     myimage := image.NewRGBA(image.Rect(0, 0, bordure, bordure))
 
     var couleur color.RGBA
 
     couleur=color.RGBA{255,255,255,255}
+
     draw.Draw(myimage, image.Rect(0, 0, bordure,bordure), &image.Uniform{couleur}, image.ZP, draw.Src)
 
 
-      for x := 0 ; x<700 ; x++{
-        i:=float64(x)/700*4-2
-        for y := 0 ; y<700 ;y++{
-            j:=float64(y)/700*4-2
+    for x := 0 ; x<Bordure ; x++{
+
+        i:=float64(x)/Bordure*(Xmax - Xmin) + Xmin
+
+        for y := 0 ; y<Bordure ;y++{
+
+            j:=float64(y)/Bordure*(Ymax - Ymin) + Ymin
+
             c := complex(i,j)
+            
             myimage.Set(x,y,mandelbrot(c))
 
 
     }
 }
            
-        
-    
-  
+    myfile, _ := os.Create(new_png_file)
 
-
-
-    myfile, _ := os.Create(new_png_file)    
     png.Encode(myfile, myimage)
+
 }
