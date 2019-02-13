598446fac6ecd45d07dfec1344685fbca03350ee
Mehdi Merimi <mehdi.78340@hotmail.fr>
2019-02-13 15:34:52 +0100

    fraction.go propre

--- exercice/structure/fraction.go
+++ exercice/structure/fraction.go
@@ -18,8 +18,11 @@ func Pgcd(a int,b int) int{
 	}
 
 	for a % m !=0 || b % m !=0 {
+
 		m--
+
 	}
+	
 	return m
 }
 
@@ -36,23 +39,31 @@ func Init_frac(num int,den int) Fraction{
 	return resultat
 	
 }
+
 func Afficher_frac(f Fraction) {
 
 	fmt.Println(f.Numerateur,"/",f.Denominateur)
 	
 }
+
 func Add_frac(f1 Fraction,f2 Fraction) Fraction{
 
 	return Init_frac(f1.Numerateur   * f2.Denominateur + f2.Numerateur * f1.Denominateur,f1.Denominateur * f2.Denominateur)
+
 }
+
 func Mult_frac(f1 Fraction,f2 Fraction) Fraction{
 
 	return Init_frac(f1.Numerateur   * f2.Numerateur,f1.Denominateur * f2.Denominateur)
+
 }
+
 func Mult_scal_frac(f1 Fraction,scalaire int) Fraction{
 
 	return Init_frac(f1.Numerateur * scalaire, f1.Denominateur)
+
 }
+
 func main() {
 
 	f1 := Init_frac(10,18)
@@ -60,6 +71,7 @@ func main() {
 	f2 := Init_frac(20,18)
 
 	Afficher_frac(f1)
+
 	Afficher_frac(f2)
 
 	Afficher_frac(Add_frac(f1,f2))
