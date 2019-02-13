99369e8aedabbd5d8a1ed4e68f416bbac8c289c9
Mehdi Merimi <mehdi.78340@hotmail.fr>
2019-02-13 15:20:29 +0100

    rendre le code du dossier tableau propre

--- exercice/dessin/rectangle.go
+++ exercice/dessin/rectangle.go
@@ -11,25 +11,22 @@ import (
 
 func main() {
 
-
-
     new_png_file := "/home/user/Bureau/Go-Language/exercice/dessin/deux_rectangles.png"
 
     myimage := image.NewRGBA(image.Rect(0, 0, 220, 220)) 
-    mygreen := color.RGBA{0, 100, 0, 255}  
 
+    mygreen := color.RGBA{0, 100, 0, 255}  
     
     draw.Draw(myimage, myimage.Bounds(), &image.Uniform{mygreen}, image.ZP, draw.Src)
 
-    red_rect := image.Rect(60, 80, 120, 160) 
+    red_rect := image.Rect(60, 80, 120, 160)
+
     myred := color.RGBA{200, 0, 0, 255}
 
-
-
-    // create a red rectangle atop the green surface
     draw.Draw(myimage, red_rect, &image.Uniform{myred}, image.ZP, draw.Src)
 
-    myfile, _ := os.Create(new_png_file)     // ... now lets save imag
+    myfile, _ := os.Create(new_png_file)
+         
     png.Encode(myfile, myimage)
 }
 
